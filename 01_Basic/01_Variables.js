d8abd0c6f421be832ebbe8b7dabe4bdb15405571
Mohak Anand <mohakanand689@gmail.com>
2025-02-26 12:16:20 +0000

    Variables And Datatype

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId=11214
+let accountEmail="mohakanand78@gmail.com"
+var accountPassword="11111" //WE WILL NOT USE THIS
+accountCity="Lucknow" //IT IS WRONG WAY TO DECLARE THE VARIABLES
+let accountState //IT IS A EMPTY VARIABLE WHICH IS UNDEFINED
+// accountId=2 //WE CANT CHANGE IT BECAUSE IT IS CONSTANT VAlUE
+accountEmail="gs@gs.com"
+accountPassword="121"
+accountCity="UP" 
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_Basic/02_Datatype.js
@@ -0,0 +1,10 @@
+"use strict" //IT TREATS ALL JS CODE IN NEW VERSION
+//alert(3+3) //WE CANNOT USE THIS IN NODE JS BECAUSE IT IS BOM OBJECT
+
+let a="MOHAK" //STRING
+let b=12 //INTEGER
+let c=true //BOOLEAN
+let d=null //NULL
+let e; //UNDEFINED
+
+console.log(typeof a); //typeof IS USE TO CHECK THE DATATYPE OF VARIABLE
