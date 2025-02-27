251add445c626b435b9eed3beabb169ef1faf6f0
Mohak Anand <mohakanand689@gmail.com>
2025-02-27 15:59:23 +0000

    Comparision

---
+++ 01_Basic/04_Comparision.js
@@ -0,0 +1,9 @@
+// AVOID THESE COMPARISION ONLY COMAPARE SAME DATA TYPE
+
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
