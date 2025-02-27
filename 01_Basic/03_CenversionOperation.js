42d149b84a4ecad2fd9068c2a576415d1d42cb72
Mohak Anand <mohakanand689@gmail.com>
2025-02-27 13:44:43 +0000

    Conversion Of Datatype

---
+++ 01_Basic/03_CenversionOperation.js
@@ -0,0 +1,44 @@
+// let score="33"
+// console.log(typeof score);
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+
+// let score="33abc"
+// console.log(typeof score);
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score=null
+// console.log(typeof score);
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score=undefined
+// console.log(typeof score);
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score=true
+// console.log(typeof score);
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score="Mohak"
+// console.log(typeof score);
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean( isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber=65
+let numberInString=String(someNumber)
+console.log(numberInString);
+console.log(typeof numberInString);
+
